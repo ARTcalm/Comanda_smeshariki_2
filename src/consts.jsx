163295887204e393e5bb2214b48f987430247e53
ARTcalm <zogin2000@mail.ru>
2026-03-10 15:37:45 +0300

    Подготовка к сборке

--- src/consts.jsx
+++ src/consts.jsx
@@ -1,4 +1,4 @@
-export const basename = "/CommandWorks/CP2/"
+export const basename = "/ARTcalm.github.io/Comanda_smeshariki_2/"
 
 export const GOODS = [
   
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/CommandWorks/CP2/",
+  base:"ARTcalm.github.io/Comanda_smeshariki_2/",
   plugins: [react()],
 })
